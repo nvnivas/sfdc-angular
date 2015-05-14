d7499f0b5287098a1a64779312c5610533bb2efe
NallapaV <NallapaV@hyrdlt2241.es.ad.adp.com>
2015-05-14 15:13:19 +0530

    testing

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var sflogin_url = process.env.sflogin_url;
 //to enable cors
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,salesforceproxy-endpoint");
   next();
 });
 
